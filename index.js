e2fddbe10eab22008c56cbfb4e45c595cd62ff76
Edgar Guzman <gamingtop@yahoo.com>
2021-07-27 17:05:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const PORT = 3000;
 
 // View Templating Engine
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-// app.use('/css', express.static(__dirname + 'public/css'));
-// app.use('/images', express.static(__dirname + 'public/images'));
-// app.use('/scripts', express.static(__dirname + 'public/scripts'));
 
 // Rerouting Files
 app.use('/', routes);
