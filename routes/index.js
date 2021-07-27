43432488d8cd686005bf6e8341eef96a090fa1f5
Edgar Guzman <gamingtop@yahoo.com>
2021-07-27 17:13:25 -0400

    idk anymore

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     // res.send('Home Page Route');
-    res.render('pages/index', {
-        message: 'EJS Home Page Route'
-    });
+    // res.render('pages/index', {
+    //     message: 'EJS Home Page Route'
+    // });
+
+    // The render method takes the name of the HTML
+    // page to be rendered as input
+    // This page should be in the views folder
+    // in the root directory.
+    res.render('index')
 });
 
 router.get('/about', (req, res) => {
